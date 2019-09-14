2393e656846709c93732b2a76d3950a60d5d3b61
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-09-14 18:51:36 +0530

    Updates

---
+++ precache-manifest.c3c6a61daefdbf9ac0614226dbdaf8de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7999cd704ba8cb1c19f0b82d776d7a1a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4b06d35ae5d769c19eae",
+    "url": "/monsters-rolodex/static/css/main.36dcb16d.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "4b06d35ae5d769c19eae",
+    "url": "/monsters-rolodex/static/js/main.4e3e2bfb.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
